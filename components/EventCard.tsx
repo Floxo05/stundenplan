212afe2a90e80812e6ba37bac8fc43a534c1da09
floxo <chris23wolf@googlemail.com>
2024-01-02 16:04:03 +0100

    show time mobile change title

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -9,21 +9,18 @@ interface EventCardProps {
 }
 
 const EventCard: React.FC<EventCardProps> = ({event, index}) => {
-    const isLaptop = useMediaQuery({minWidth: 992})
 
     return (
         <td key={index}>
             {event ? (
                 <div className={"p-2 bg-gray-500 rounded"}>
                     <p className="text-xl font-bold">{event.description}</p>
-                    <p className="text-gray-400">{event.instructor}</p>
-                    <p className="text-gray-400">{event.room}</p>
-                    {isLaptop && (
-                        <p className="text-gray-400">{`Start: ${format(new Date(event.date), 'HH:mm')} | End: ${format(
+                    <p className="">{event.instructor}</p>
+                    <p className="">{event.room}</p>
+                        <p className="">{`Start: ${format(new Date(event.date), 'HH:mm')} | End: ${format(
                         new Date(event.dateEnd),
                         'HH:mm'
                     )}`}</p>
-                    )}
                 </div>
             ) : (
                 <div>-</div>
